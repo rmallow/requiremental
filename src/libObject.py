8ee4a8a8466630fe2ab4926e16a83c3be8ee34e3
rmallow <rmallow@crimson.ua.edu>
2020-09-14 23:49:26 -0500

    removing setup from git and adding details to libObject

--- src/libObject.py
+++ src/libObject.py
@@ -1,12 +1,17 @@
 import parser
+import collections
 
 class libObject:
-    def __init__(self, obj):
+    def __init__(self, obj, details = None):
         self.m_obj = obj
         self.m_module = None
         self.m_name = None
+        self.m_details = {}
         self.m_reqs = set()
         if self.m_obj is not None:
             if obj.__module__ is not None:
                 self.m_module = parser.safeGetMoudle(obj.__module__)
             self.m_name = obj.__name__
+
+        if isinstance(details, collections.Mapping):
+            self.m_details = details
